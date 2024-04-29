b8658b2fa3caa8a1aed752b8980327bda8ffe3ab
Isaque Barbosa <isaquebatista400@gmail.com>
2024-04-28 22:09:47 -0300

    Buttons

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components'
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #cdcdcd;
+  backgound-color: #00AAFF.
+  color: #FFFFFF;
+  font-size: 24px;
+  font-weight: 700px;
+  flex: 1;
+`
